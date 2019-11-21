ef8254bf1957dd8da8a4847515bf87780dd5d1b1
VictorMeneghini <vhmeneghini@gmail.com>
2019-11-21 17:18:34 -0300

    Create and redirect to step 1

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/views/Home';
-import LandingPage from '@/views/LandingPage';
-import SignUp from '@/views/auth/SignUp';
-import MyProfile from '@/components-v1/MyProfile';
-import ResetPassword from '@/components-v1/ResetPassword';
-import RepositoryHome from '@/views/repository/Home';
-import RepositoryTrainings from '@/views/repository/Trainings';
-import RepositoryTranslate from '@/views/repository/Translate';
-import RepositoryTranslations from '@/views/repository/Translations';
-import RepositorySettings from '@/views/repository/Settings';
-import RepositoryAnalyzeText from '@/views/repository/AnalyzeText';
-import RepositoryEvaluate from '@/views/repository/Evaluate';
-import NotFound from '@/views/NotFound';
-import SafariAlert from '@/views/SafariAlert';
+// import Home from '@/views/Home';
+// import LandingPage from '@/views/LandingPage';
+// import SignUp from '@/views/auth/SignUp';
+// import MyProfile from '@/components-v1/MyProfile';
+// import ResetPassword from '@/components-v1/ResetPassword';
+// import RepositoryHome from '@/views/repository/Home';
+// import RepositoryTrainings from '@/views/repository/Trainings';
+// import RepositoryTranslate from '@/views/repository/Translate';
+// import RepositoryTranslations from '@/views/repository/Translations';
+// import RepositorySettings from '@/views/repository/Settings';
+// import RepositoryAnalyzeText from '@/views/repository/AnalyzeText';
+// import RepositoryEvaluate from '@/views/repository/Evaluate';
+// import NotFound from '@/views/NotFound';
+// import SafariAlert from '@/views/SafariAlert';
 import store from '../store';
 
+// wireframe import
+import step1 from '@/views/wireframe/step1';
 
 Vue.use(Router);
 
@@ -23,98 +25,91 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'landingPage',
-      component: LandingPage,
-      beforeEnter: async (to, from, next) => {
-        if (store.getters.authenticated) {
-          next('/home');
-        } else {
-          next();
-        }
-      },
+      path: '',
+      name: 'step1',
+      component: step1,
     },
-    {
-      path: '/signup',
-      name: 'signUp',
-      component: SignUp,
-      beforeEnter: async (to, from, next) => {
-        if (store.getters.authenticated) {
-          next('/home');
-        } else {
-          next();
-        }
-      },
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/reset-password/:nickname/:token/',
-      component: ResetPassword,
-    },
-    {
-      path: '/myprofile/',
-      name: 'myProfile',
-      component: MyProfile,
-      beforeEnter: async (to, from, next) => {
-        if (!store.getters.authenticated) {
-          store.dispatch('openLoginModal', {
-            next: to,
-            redirectToWhenFails: { name: 'home' },
-          });
-        } else {
-          next();
-        }
-      },
-    },
-    {
-      path: '/:ownerNickname/:slug/',
-      name: 'repository-summary',
-      component: RepositoryHome,
-    },
-    {
-      path: '/:ownerNickname/:slug/training/',
-      name: 'repository-training',
-      component: RepositoryTrainings,
-    },
-    {
-      path: '/:ownerNickname/:slug/translate/',
-      name: 'repository-translate',
-      component: RepositoryTranslate,
-    },
-    {
-      path: '/:ownerNickname/:slug/translations/',
-      name: 'repository-translations-status',
-      component: RepositoryTranslations,
-    },
-    {
-      path: '/:ownerNickname/:slug/Integration/',
-      name: 'repository-integration',
-      component: RepositoryAnalyzeText,
-    },
-    {
-      path: '/:ownerNickname/:slug/settings/',
-      name: 'repository-settings',
-      component: RepositorySettings,
-    },
-    {
-      path: '/:ownerNickname/:slug/evaluate/',
-      name: 'repository-test',
-      component: RepositoryEvaluate,
-    },
-    {
-      path: '*',
-      name: '404',
-      component: NotFound,
-    },
-    {
-      path: '/safariAlert/',
-      name: 'safari-alert',
-      component: SafariAlert,
-    },
+    // {
+    //   path: '/signup',
+    //   name: 'signUp',
+    //   component: SignUp,
+    //   beforeEnter: async (to, from, next) => {
+    //     if (store.getters.authenticated) {
+    //       next('/home');
+    //     } else {
+    //       next();
+    //     }
+    //   },
+    // },
+    // {
+    //   path: '/home',
+    //   name: 'home',
+    //   component: Home,
+    // },
+    // {
+    //   path: '/reset-password/:nickname/:token/',
+    //   component: ResetPassword,
+    // },
+    // {
+    //   path: '/myprofile/',
+    //   name: 'myProfile',
+    //   component: MyProfile,
+    //   beforeEnter: async (to, from, next) => {
+    //     if (!store.getters.authenticated) {
+    //       store.dispatch('openLoginModal', {
+    //         next: to,
+    //         redirectToWhenFails: { name: 'home' },
+    //       });
+    //     } else {
+    //       next();
+    //     }
+    //   },
+    // },
+    // {
+    //   path: '/:ownerNickname/:slug/',
+    //   name: 'repository-summary',
+    //   component: RepositoryHome,
+    // },
+    // {
+    //   path: '/:ownerNickname/:slug/training/',
+    //   name: 'repository-training',
+    //   component: RepositoryTrainings,
+    // },
+    // {
+    //   path: '/:ownerNickname/:slug/translate/',
+    //   name: 'repository-translate',
+    //   component: RepositoryTranslate,
+    // },
+    // {
+    //   path: '/:ownerNickname/:slug/translations/',
+    //   name: 'repository-translations-status',
+    //   component: RepositoryTranslations,
+    // },
+    // {
+    //   path: '/:ownerNickname/:slug/Integration/',
+    //   name: 'repository-integration',
+    //   component: RepositoryAnalyzeText,
+    // },
+    // {
+    //   path: '/:ownerNickname/:slug/settings/',
+    //   name: 'repository-settings',
+    //   component: RepositorySettings,
+    // },
+    // {
+    //   path: '/:ownerNickname/:slug/evaluate/',
+    //   name: 'repository-test',
+    //   component: RepositoryEvaluate,
+    // },
+    // {
+    //   path: '*',
+    //   name: '404',
+    //   component: NotFound,
+    // },
+    // {
+    //   path: '/safariAlert/',
+    //   name: 'safari-alert',
+    //   component: SafariAlert,
+    // },
   ],
   scrollBehavior() {
     return { x: 0, y: 0 };
---
+++ src/views/wireframe/step1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav
+      class="navbar"
+      role="navigation"
+      aria-label="main navigation bg-navbar"/>
+    <section class="section a">
+      <div class="container">
+        <div class="columns is-mobile">
+          <div class="column is-11 is-offset-1"/>
+        </div>
+        <h1 class="title">bothub</h1>
+        <h2 class="subtitle">
+          bothub icons here
+        </h2>
+      </div>
+    </section>
+    <footer class="footer">
+      <div class="content has-text-centered"/>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.navbar, .footer {
+  background-color: #6f1ab9 !important;
+}
+</style>
